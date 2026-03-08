b49853dcd4d7c0d0996a915f0af8f152bfd0864b
smef0812-droid <smef0812@gmail.com>
2026-03-08 16:47:04 +0900

    sum

---
+++ kadai_005/score.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<body>
+    <p>
+        <?php
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+
+        $sum = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+        $ave = $sum / 10;
+        echo "合計は{$sum}です。<br>";
+        echo "平均は{$ave}です。";
+        ?>
+</p>
+</body>
+</html>
