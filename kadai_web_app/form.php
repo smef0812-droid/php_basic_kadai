7b6882034a8d9da09d12df620707a8bab5ece683
smef0812-droid <smef0812@gmail.com>
2026-03-11 22:47:58 +0900

    Add files via upload

---
+++ kadai_web_app/form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>社員フォーム</title>
+  </head> 
+
+  <body>
+    <h2>社員情報入力フォーム</h2>
+    <!-- 入力フォーム -->
+    <form action="confirm.php" method="POST">
+        <table>
+         <tr>
+          <td>社員名</td>
+          <td><input type="text" name="employee_name" value="a"> </td>
+         </tr>
+         <tr>
+          <td>年齢</td>
+          <td><input type="number" name="employee_age" value="b"> </td>
+         </tr>
+         <tr>
+          <td>所属</td>
+          <td>
+          <select name="department">
+           <option value="開発部">開発部</option>
+           <option value="営業部">営業部</option>
+           <option value="人事部">人事部</option>
+          </select>
+          </td>
+         </tr>
+        </table>
+           <input type="submit" value="送信">
+    </form>
+
+  </body>
+</html>
